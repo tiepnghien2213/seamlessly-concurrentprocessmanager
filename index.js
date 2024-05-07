61081b8386599ebfec3965336e57aa20790ab2e7
tiepnghien2213 <littlelurknam1311@outlook.com>
2024-05-07 13:46:22 +0700

    update commit

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) return [];
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-  const result = [];
-  backtrack("", 0);
-  return result;
-  function backtrack(current, index) {
-    if (current.length === digits.length) {
-      result.push(current);
-      return;
-    }
-    const letters = map[digits[index]];
-    for (const letter of letters) {
-      backtrack(current + letter, index + 1);
+function maximalSquare(matrix) {
+  if (matrix.length === 0 || matrix[0].length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from({ length: rows + 1 }, () =>
+    Array.from({ length: cols + 1 }, () => 0),
+  );
+  let maxSquare = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+        maxSquare = Math.max(maxSquare, dp[i][j]);
+      }
     }
   }
+  return maxSquare * maxSquare;
 }
