48ea72c8e71aa39d91221d3cfafb7cbdf55adfb6
tiepnghien2213 <littlelurknam1311@outlook.com>
2024-05-10 03:49:32 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const randomNumber = Math.floor(Math.random() * 10) + 1;
+function levelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(currentLevel);
+  }
+  return result;
+}
