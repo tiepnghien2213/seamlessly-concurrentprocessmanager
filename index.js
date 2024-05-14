33801cd160c3ce832284e4e3b3e88a03b93d3068
tiepnghien2213 <littlelurknam1311@outlook.com>
2024-05-14 07:41:33 +0700

    init project

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function averageArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
-}
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
